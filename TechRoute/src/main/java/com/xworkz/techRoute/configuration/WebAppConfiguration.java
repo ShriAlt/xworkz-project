3e354f1d0914449ccdcbfa7e0afa3b98056157b8
Shriharsha K M <shriharshakm10@gmail.com>
2025-10-08 23:37:50 +0530

    Update .gitignore to prevent committing application-secret.properties

--- TechRoute/src/main/java/com/xworkz/techRoute/configuration/WebAppConfiguration.java
+++ TechRoute/src/main/java/com/xworkz/techRoute/configuration/WebAppConfiguration.java
@@ -3,10 +3,15 @@ package com.xworkz.techRoute.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.io.IOException;
 
 @Configuration
 @ComponentScan(basePackages = "com.xworkz.techRoute")
@@ -26,4 +31,15 @@ public class WebAppConfiguration implements WebMvcConfigurer {
     {
         return new InternalResourceViewResolver("/",".jsp");
     }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() throws IOException {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        Resource[] resources = new Resource[] {
+                new ClassPathResource("application.properties"),
+                new ClassPathResource("application-secret.properties")
+        };
+        configurer.setLocations(resources);
+        return configurer;
+    }
+
 }
