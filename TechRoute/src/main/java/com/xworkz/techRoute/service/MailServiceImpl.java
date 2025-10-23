8cb20ae28a4738e40f6382152f9bd475d77c62b5
Shriharsha K M <shriharshakm10@gmail.com>
2025-10-23 23:22:01 +0530

    update on admin logic

--- TechRoute/src/main/java/com/xworkz/techRoute/service/MailServiceImpl.java
+++ TechRoute/src/main/java/com/xworkz/techRoute/service/MailServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.Random;
 
 @Service
 public class MailServiceImpl implements MailService{
-
     public MailServiceImpl(){
         System.out.println("no args of MailService");
     }
