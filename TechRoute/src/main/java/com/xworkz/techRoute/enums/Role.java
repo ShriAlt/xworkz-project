0391a65ea3edfa110de885f8638663563616c5bc
Shriharsha K M <shriharshakm10@gmail.com>
2025-11-03 18:27:40 +0530

    update

--- TechRoute/src/main/java/com/xworkz/techRoute/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.techRoute.enums;
-
-public enum Role {
-    ADMIN , USER;
-}
--- TechRoute/src/main/java/com/xworkz/techRoute/service/MailService.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.techRoute.service;
-
-public interface MailService {
-
-    boolean sendOtp(String toEmail , String otp);
-}
